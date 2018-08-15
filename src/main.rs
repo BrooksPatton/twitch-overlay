dbb4c3451ce7b7391da88f232d51fe94bee7ebe0
Brooks Patton <brooks@brookzerker.com>
2018-08-15 07:10:26 -0600

    added quick break route

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,22 @@ extern crate actix_web;
 
 use actix_web::{server, App, HttpRequest, HttpResponse, fs};
 
-fn greet(_req: &HttpRequest) -> HttpResponse {
+fn starting_soon(_req: &HttpRequest) -> HttpResponse {
     HttpResponse::Ok()
         .body(include_str!("../static/index.html"))
+}
+
+fn break_time(_req: &HttpRequest) -> HttpResponse {
+    HttpResponse::Ok()
+        .body(include_str!("../static/break_time.html"))
 }
 
 fn main() {
     server::new(|| {
         App::new()
             .handler("/static", fs::StaticFiles::new("./static").unwrap())
-            .resource("/", |r| r.f(greet))  
+            .resource("/", |r| r.f(starting_soon))
+            .resource("/break", |r| r.f(break_time))
     })
     .bind("127.0.0.1:3001")
     .expect("Could not initiate web server")
