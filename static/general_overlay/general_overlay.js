d6cb360ca954407080d52b3c6e72b35a38ca71d7
Brooks Patton <brooks@brookzerker.com>
2018-08-23 08:33:50 -0600

    created chat overlay

--- static/general_overlay/general_overlay.js
+++ static/general_overlay/general_overlay.js
@@ -3,17 +3,16 @@
     // Create WebSocket connection.
     const socket = new WebSocket('ws://localhost:3002');
 
-    // Connection opened
-    socket.addEventListener('open', function (event) {
-        socket.send(JSON.stringify({message: "front end starting up"}));
-    });
-
     // Listen for messages
     socket.addEventListener('message', function (event) {
         const message = JSON.parse(event.data);
         console.log('Message from server ', message);
         generateTag(message)
           .then(injectIntoDom)
+          .then(fadeNodeIn)
+          .then(delay)
+          .then(fadeNodeOut)
+          .then(addEndingEventListener)
     });
 
     function generateTag({nickname, message}) {
@@ -31,4 +30,32 @@
 
     function injectIntoDom(tag) {
       chatDiv.appendChild(tag)
+
+      return tag
+    }
+
+    function fadeNodeIn(tag) {
+      tag.classList.add('fade-in')
+
+      return tag
+    }
+
+    function delay(tag) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(tag)
+        }, 10000)
+      })
+    }
+
+    function fadeNodeOut(tag) {
+      tag.classList.add('fade-out')
+
+      return tag
+    }
+
+    function addEndingEventListener(tag) {
+      tag.addEventListener('animationend', function(event) {
+        tag.remove()
+      })
     }
